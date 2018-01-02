d373806923a911a4066f40e952f16a9b8eecea11
Nolan Byrnes <nolanehs@gmail.com>
2018-01-02 17:31:12 +0000

    automatically backed up by learn

--- slideshower/lib/slideshower/search.rb
+++ slideshower/lib/slideshower/search.rb
@@ -15,10 +15,10 @@ def self.scrape
   doc = Nokogiri::HTML(open("#{@pictures}"))
 
   doc.css(".photo-item a").each do |foto|
-  picture = Photo.new
-  picture.name = foto.attribute("title")
-  picture.url = foto.attribute("href").value
-  end
+    picture = Photo.new
+    picture.name = foto.attribute("title")
+    picture.url = foto.attribute("href").value
+  end #=> doc.css(".photo-item a")
 end
 
 end #of Class
